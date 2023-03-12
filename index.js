718bcfb664e2c48bf07f9ef4baaa5af0aabb94b7
sustapakhreen <sushta48@gmail.com>
2023-03-12 12:57:24 -0700

    DOM modifying elements lab complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+
+newHeader.innerHTML = "Coco is the champion";
